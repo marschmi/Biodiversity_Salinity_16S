c6770786d326ab279556d6c7ddcbdfa4d80cf306
Marian Schmidt <marschmi@cornell.edu>
2026-03-25 13:41:58 -0400

    updates to biodiv lesson

---
+++ code/colors.R
@@ -0,0 +1,66 @@
+# Setting colors for stations 
+station_colors <- c(
+  "Shipping Channel" = "dodgerblue4",
+  "Aransas Bay" = "dodgerblue2",
+  "Copano West" = "#D9CC3C",
+  "Copano East" = "#A0E0BA",
+  "Mesquite Bay" = "#00ADA7")
+
+
+
+# Set the phylum colors
+phylum_colors <- c(
+  Acidobacteriota = "navy", 
+  Actinomycetota = "darkslategray2", 
+  Armatimonadota = "deeppink1",
+  Alphaproteobacteria = "plum2", 
+  Bacteroidota = "gold", 
+  Betaproteobacteria = "plum1", 
+  Bdellovibrionota = "red1",
+  Chloroflexota="black", 
+  "Candidatus Kapabacteria" = "firebrick",
+  Cyanobacteriota = "limegreen",
+  Deltaproteobacteria = "grey", 
+  Desulfobacterota="magenta",
+  Firmicutes = "#3E9B96",
+  Gammaproteobacteria = "greenyellow",
+  "Marinimicrobia (SAR406 clade)" = "yellow",
+  Myxococcota = "#B5D6AA",
+  Pseudomonadota = "royalblue",
+  Planctomycetota = "darkorange", 
+  "SAR324 clade(Marine group B)" = "olivedrab",
+  Proteobacteria_unclassified = "greenyellow",
+  Thermoplasmatota = "green",
+  Thermoproteota = "palevioletred1",
+  Verrucomicrobiota = "darkorchid1",
+  Other = "grey")
+
+
+
+
+phylum_gus_colors <- c(
+  Acidobacteriota = "navy", 
+  Actinomycetota = "darkslategray2", 
+  Armatimonadota = "deeppink1",
+  Alphaproteobacteria = "plum2", 
+  Bacteroidota = "gold", 
+  Betaproteobacteria = "plum1", 
+  Bdellovibrionota = "red1",
+  Chloroflexota="black", 
+  Crenarchaeota = "yellow",
+  Cyanobacteriota = "limegreen",
+  Deltaproteobacteria = "grey", 
+  Desulfobacterota="magenta",
+  Firmicutes = "#3E9B96",
+  Gammaproteobacteria = "greenyellow",
+  Patescibacteria = "yellow",
+  Myxococcota = "#B5D6AA",
+  #Nitrospirota = "palevioletred1",
+  Pseudomonadota = "palevioletred1",
+  Planctomycetota = "darkorange", 
+  "SAR324 clade(Marine group B)" = "olivedrab",
+  Gemmatimonadota = "greenyellow",
+  Thermodesulfobacteriota = "royalblue",
+  Verrucomicrobiota = "darkorchid1",
+  Other = "grey",
+  Rare = "grey")
