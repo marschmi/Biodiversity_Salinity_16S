15022aa2b1960a24b00a30a34c9a81cfa23adc8b
Marian Schmidt <marschmi@cornell.edu>
2026-04-08 13:15:21 -0400

    fixing paths again oops

--- code/colors_and_shapes.R
+++ code/colors_and_shapes.R
@@ -1,12 +1,52 @@
 # Setting colors for stations 
+############# SHAPES AND COLORS 
+###### SHAPES 
+date_shapes <- c(
+  "6/2/21" = 15, 
+  "6/15/21" = 16, 
+  "10/5/21" = 17)
+
+date1_shapes <- c(
+  "2021-06-02" = 15, 
+  "2021-06-15" = 16, 
+  "2021-10-05" = 17)
+
+depth_cat_shapes <- c(
+  "surface" = 18, 
+  "bottom" = 20)
+
+fraction_shapes <- c(
+  "Particle" = 23, 
+  "Free" = 21,
+  "Whole" = 22,
+  "Control" = 24)
+
+salinity_shapes <- c(
+  "Very Low" = 21,   # 
+  "Low"      = 22,   # 
+  "Mid"      = 23,   #  
+  "High"     = 24)    #  
+
+####### COLORS 
+# Update colors for salinity groups
+salinity_colors <- c(
+  "Very Low" = "#A8E6CE",   # light mint
+  "Low"      = "#56C1D3",   # aqua
+  "Mid"      = "#1E90FF",   # dodger blue
+  "High"     = "#08306B")    # deep navy
+
 station_colors <- c(
-  "Shipping Channel" = "dodgerblue4",
-  "Aransas Bay" = "dodgerblue2",
-  "Copano West" = "#D9CC3C",
-  "Copano East" = "#A0E0BA",
-  "Mesquite Bay" = "#00ADA7")
+  "Shipping Channel" = "#3F007D",  # Deep purple
+  "Aransas Bay"      = "#5E2D91",  # Vivid indigo
+  "Mesquite Bay"     = "#8831A4",  # Strong purple-magenta
+  "Copano East"      = "#B63689",  # Bright fuchsia
+  "Copano West"      = "#D72576")   # Bold magenta
 
-
+fraction_colors <- c(
+  "Particle" = "firebrick3", 
+  "Free" = "goldenrod1", 
+  "Whole" = "darkorange2",
+  "Control" = "grey")
 
 # Set the phylum colors
 phylum_colors <- c(
